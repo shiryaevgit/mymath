08c84958a59d97776c43908fc130b31405693c4a
egorofgit <euro24rus@gmail.com>
2023-10-12 11:05:32 +0300

    first commit

---
+++ mymath.go
@@ -0,0 +1,39 @@
+package mymath
+
+import (
+	"math"
+)
+
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+
+func Floorr(x float64) float64 {
+	return math.Floor(x)
+}
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+}
